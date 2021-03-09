668f30936093e7e1d69653afd3d93d7d977b6e6a
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-09 08:04:33 -0600

    creating classes exercise

---
+++ src/TextBox.java
@@ -0,0 +1,12 @@
+public class TextBox {
+
+    public String text = ""; // field
+
+    public void setText(String text){ // void is the return type because you don't want to return values here
+        this.text = text; // when the name of a parameter is the same as the name of the field you can use
+        // the this keyword. this keyword is a reference to the current object. this.text = value of parameter
+    }
+    public void clear() {
+       text = "";
+    }
+}
