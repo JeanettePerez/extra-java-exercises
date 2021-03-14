774c6ac9031ed8e7158d275a4c2d6c10e9159012
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-14 18:39:47 -0500

    testing out inheritance by extending UIControl to TextBox

--- src/TextBox.java
+++ src/TextBox.java
@@ -1,6 +1,11 @@
-public class TextBox {
+public class TextBox extends UIControl {
 
-    public String text = ""; // field
+    private String text = ""; // field
+
+    public TextBox() {
+        super(true);
+        System.out.println("TextBox");
+    }
 
     public void setText(String text){ // void is the return type because you don't want to return values here
         this.text = text; // when the name of a parameter is the same as the name of the field you can use
