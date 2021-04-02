0ad43fa1471743da1b024d1fbb49285fe7aaf8a1
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-02 14:59:20 -0500

    using run method to write conditions for running a thread

--- src/concurrency/DownloadFileTask.java
+++ src/concurrency/DownloadFileTask.java
@@ -1,4 +1,23 @@
 package concurrency;
 
-public class DownloadFileTask {
+public class DownloadFileTask implements Runnable{
+    @Override
+    public void run() {
+//        System.out.println("Downloading a file: " + Thread.currentThread().getName()); // placeholder for the time being
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Download complete " + Thread.currentThread().getName());
+        // === interrupting thread
+        System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+            for(var i = 0; i < Integer.MAX_VALUE; i++) {
+                if(Thread.currentThread().isInterrupted()) return; // if this statement returns true break or return
+                System.out.println("Downloading byte " + i); // else download the byte
+            }
+
+        System.out.println("Download complete " + Thread.currentThread().getName());
+    }
 }
