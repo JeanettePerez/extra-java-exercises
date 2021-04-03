ea3c0f044e5c62c33698b334d00e4a87dc193384
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-03 14:14:57 -0500

    overloading simulate method

--- src/executors/CompletableFutureDemo.java
+++ src/executors/CompletableFutureDemo.java
@@ -1,6 +1,9 @@
 package executors;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class CompletableFutureDemo {
     // =========== Transforming a Completable Future ========
@@ -45,9 +48,18 @@ public class CompletableFutureDemo {
 //        var rate = CompletableFuture.supplyAsync(() -> 0.9);
 //        USD.thenCombine(rate,(price, exchangeRate) -> price * exchangeRate).thenAccept(System.out::println);
 // === Price Finder ===
+        var start = LocalTime.now();
         var service = new FlightService();
-        service.getQuote("site1")
-                .thenAccept(System.out::println);
+        var futures = service.getQuotes()
+                .map(future -> future.thenAccept(System.out::println))
+                .collect(Collectors.toList());
+        CompletableFuture
+                .allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> {
+                    var end = LocalTime.now();
+                    var duration = Duration.between(start,end);
+                    System.out.println("Retrieved all quotes in " + duration.toMillis() + " msecs");
+                });
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
