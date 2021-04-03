aa825a26df47f8cb1415cb55ba62169219e97cbe
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-03 14:16:39 -0500

    redoing price finder for practice

--- src/priceFinder/MainPriceFinder.java
+++ src/priceFinder/MainPriceFinder.java
@@ -1,12 +1,26 @@
 package priceFinder;
 
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class MainPriceFinder {
     public static void main(String[] args) {
+        var start = LocalTime.now();
         var service = new HotelService();
-        service.getQuote("site1").thenAccept(System.out::println);
-
+        var futures = service
+                .getQuotes()
+                .map(future -> future.thenAccept(System.out::println))
+                .collect(Collectors.toList());
+        CompletableFuture
+                .allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> {
+                    var end = LocalTime.now();
+                    var duration = Duration.between(start,end);
+                    System.out.println("Retrieved all quotes in " + duration.toMillis() + "msecs");
+                });
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
